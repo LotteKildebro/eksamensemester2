74ea2d22ce4f0ae5e7ec743be23932598442ae2e
david-jrh <david-hj@hotmail.com>
2020-05-22 18:11:38 +0200

    videoquiz setup done

--- script/quiz.js
+++ script/quiz.js
@@ -1,69 +1,60 @@
-function submitQuiz() {
-    console.log('submitted');
+$(document).ready(function () {
+    var pos = 0,
+        slides = $('.slide'),
+        numOfSlides = slides.length;
 
-// get each answer score
-    function answerScore (qName) {
-        var radiosNo = document.getElementsByName(qName);
-
-        for (var i = 0, length = radiosNo.length; i < length; i++) {
-               if (radiosNo[i].checked) {
-        // do something with radiosNo
-                var answerValue = Number(radiosNo[i].value);
-            }
-        }
-        // change NaNs to zero
-        if (isNaN(answerValue)) {
-            answerValue = 0;
-        }
-        return answerValue;
+    function nextSlide() {
+        // `[]` returns a vanilla DOM object from a jQuery object/collection
+        slides[pos].video.stopVideo()
+        slides.eq(pos).animate({ left: '-100%' }, 500);
+        pos = (pos >= numOfSlides - 1 ? 0 : ++pos);
+        slides.eq(pos).css({ left: '100%' }).animate({ left: 0 }, 500);
     }
 
-// calc score with answerScore function
-    var calcScore = (answerScore('q1') + answerScore('q2') + answerScore('q3') + answerScore('q4'));
-    console.log("CalcScore: " + calcScore); // it works!
-
-// function to return correct answer string
-    function correctAnswer (correctStringNo, qNumber) {
-        console.log("qNumber: " + qNumber);  // logs 1,2,3,4 after called below
-        return ("The correct answer for question #" + qNumber + ": &nbsp;<strong>" +
-            (document.getElementById(correctStringNo).innerHTML) + "</strong>");
-        }
-
-// print correct answers only if wrong (calls correctAnswer function)
-    if (answerScore('q1') === 0) {
-        document.getElementById('correctAnswer1').innerHTML = correctAnswer('correctString1', 1);
-    }
-    if (answerScore('q2') === 0) {
-        document.getElementById('correctAnswer2').innerHTML = correctAnswer('correctString2', 2);
-    }
-    if (answerScore('q3') === 0) {
-        document.getElementById('correctAnswer3').innerHTML = correctAnswer('correctString3', 3);
-    }
-    if (answerScore('q4') === 0) {
-        document.getElementById('correctAnswer4').innerHTML = correctAnswer('correctString4', 4);
+    function previousSlide() {
+        slides[pos].video.stopVideo()
+        slides.eq(pos).animate({ left: '100%' }, 500);
+        pos = (pos == 0 ? numOfSlides - 1 : --pos);
+        slides.eq(pos).css({ left: '-100%' }).animate({ left: 0 }, 500);
     }
 
-// calculate "possible score" integer
-    var questionCountArray = document.getElementsByClassName('question');
+    $('.left').click(previousSlide);
+    $('.right').click(nextSlide);
+})
 
-    var questionCounter = 0;
-    for (var i = 0, length = questionCountArray.length; i < length; i++) {
-        questionCounter++;
-    }
-
-// show score as "score/possible score"
-    var showScore = "Your Score: " + calcScore +"/" + questionCounter;
-// if 4/4, "perfect score!"
-    if (calcScore === questionCounter) {
-        showScore = showScore + "&nbsp; <strong>Perfect Score!</strong>"
-    };
-    document.getElementById('userScore').innerHTML = showScore;
+function onYouTubeIframeAPIReady() {
+    $('.slide').each(function (index, slide) {
+        // Get the `.video` element inside each `.slide`
+        var iframe = $(slide).find('.video')[0]
+        // Create a new YT.Player from the iFrame, and store it on the `.slide` DOM object
+        slide.video = new YT.Player(iframe)
+    })
 }
 
-$(document).ready(function() {
 
-$('#submitButton').click(function() {
-    $(this).addClass('hide');
-});
+var slideIndex = 1;
+showSlides(slideIndex);
 
-});
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
