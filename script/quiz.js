bb8a059328fb77441c07ee455a1d003e291d3533
david-jrh <david-hj@hotmail.com>
2020-05-28 03:28:07 +0200

    lidt af det hele

--- script/quiz.js
+++ script/quiz.js
@@ -1,3 +1,23 @@
+//Get the button
+var mybutton = document.getElementById("topBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+
 function overlay() {
 
     if(!$('.historie-box').is(':visible')){
